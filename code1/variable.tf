edc26e88a96f5cf6b501be59165868a6da35b760
Caroline_El <modupeoduyoye@gmail.com>
2023-07-11 21:12:59 -0500

    terraform-code

---
+++ code1/variable.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+    type = string
+    description = "size of the instance"
+  default = "t2.micro"
+}
---
+++ datasource/data.tf
@@ -0,0 +1,4 @@
+data "aws_instance" "remote-ec2" {
+  instance_id = "i-0a2db89acbe469ae8"
+
+}
